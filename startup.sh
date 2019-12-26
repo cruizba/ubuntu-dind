9a56244260e26656ffa82c0b95c72a6cbbc92028
cruizba <charlesrb94@outlook.com>
2019-12-26 16:29:20 +0000

    Minor fix

--- startup.sh
+++ startup.sh
@@ -6,7 +6,7 @@ function wait_for_process () {
     local process_name="$1"
     local waited_sec=0
     while ! pgrep "$process_name" >/dev/null && ((waited_sec < max_time_wait)); do
-        INFO "Process $process_name is not running yet. Retrying in 5 seconds"
+        INFO "Process $process_name is not running yet. Retrying in 1 seconds"
         INFO "Waited $waited_sec seconds of $max_time_wait seconds"
         sleep 1
         ((waited_sec=waited_sec+1))
