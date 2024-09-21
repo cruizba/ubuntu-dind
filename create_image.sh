6a05d0008481edbb8ec5e8e379ec0f8883b9619d
cruizba <carlos.ruizbal@gmail.com>
2024-09-22 01:50:39 +0200

    Bump versions. Docker: 27.3.0, Compose: 2.29.6, Build: 0.17.1

--- create_image.sh
+++ create_image.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-DOCKER_VERSION="27.2.1"
+DOCKER_VERSION="27.3.0"
 BUILD_NUMBER="0"
 LATEST_UBUNTU_VERSION="24.04"
 
@@ -13,16 +13,16 @@ declare -A ubuntu_versions=(
 build_image() {
     local ubuntu_version=$1
     local ubuntu_name=$2
-    
+
     echo "Building image for Ubuntu ${ubuntu_version} (${ubuntu_name})"
-    
+
     docker build \
         --build-arg UBUNTU_VERSION=${ubuntu_version} \
         -t cruizba/ubuntu-dind:${ubuntu_name}-${DOCKER_VERSION} \
         -t cruizba/ubuntu-dind:${ubuntu_name}-${DOCKER_VERSION}-r${BUILD_NUMBER} \
         -t cruizba/ubuntu-dind:${ubuntu_name}-latest \
         -f Dockerfile .
-    
+
     if [ "${ubuntu_version}" == "${LATEST_UBUNTU_VERSION}" ]; then
         docker tag cruizba/ubuntu-dind:${ubuntu_name}-latest cruizba/ubuntu-dind:latest
     fi
